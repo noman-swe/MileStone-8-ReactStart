bf0d93532e523e93b7e750650623c1870a8c3e3f
noman-swe <noman.swe130@gmail.com>
2023-03-17 23:18:37 +0600

    45-3 Create Component, return HTML from a component

--- Module-44/reactjs-core-concepts/src/App.js
+++ Module-44/reactjs-core-concepts/src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 const name = 'Shibly';
-const singer= {name: 'Robindranath Thakur', job: 'Singer'}
-const singer2= {name: 'Shanto', job: 'Singer'}
+const singer = { name: 'Robindranath Thakur', job: 'Singer' }
+const singer2 = { name: 'Shanto', job: 'Singer' }
 
 const singerStyle = {
   color: 'purple',
@@ -13,35 +13,34 @@ const singerStyle = {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
-        <div className="container">
-          <h3>Hello dude react! how are you?</h3>
-        </div>
+      <Person></Person>
+      <Person></Person>
+      <h5>New component</h5>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
 
-        <div className="music">
-          <p>Name: {'Noman ' + name}</p>
-          <p>Name: {singer.name}</p>
-          <p style={singerStyle}>Name: {singer2.name}</p>
-          <p style={{color:'yellow', backgroundColor: 'tomato'}}>Job: {singer2.job}</p>
-          
-        </div>
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Earn React App
-        </a>
-      </header>
     </div>
   );
 }
+
+function Person() {
+  return (
+    <div className="person ">
+      <h1>Sakib Al Hasan</h1>
+      <p>Profession: Cricketer</p>
+    </div>
+  );
+}
+
+function Friend() {
+  return (
+    <div className="myFriend container">
+      <h3>Name: Ajoy</h3>
+      <p>Job: Maramari</p>
+    </div>
+  )
+}
+
 
 export default App;
