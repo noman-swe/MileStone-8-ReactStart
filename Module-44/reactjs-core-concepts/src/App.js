cde1ab5dd9fa2b37da4de0edf6e6b9551d609e56
noman-swe <noman.swe130@gmail.com>
2023-03-18 00:56:40 +0600

    45-5 (advanced) Create multiple components from an array of objects

--- Module-44/reactjs-core-concepts/src/App.js
+++ Module-44/reactjs-core-concepts/src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const name = 'Shibly';
-const singer = { name: 'Robindranath Thakur', job: 'Singer' }
-const singer2 = { name: 'Shanto', job: 'Singer' }
+const singers = [
+  { name: 'Robindranath Thakur', job: 'Singer' },
+  { name: 'Shanto', job: 'Singer' },
+  { name: 'Kazi Nazrul', job: 'Chol Chol' },
+  { name: 'Mixed', job: 'sabbash Bd' }
+]
 
 const singerStyle = {
   color: 'purple',
@@ -11,25 +15,40 @@ const singerStyle = {
 }
 
 function App() {
+  const cricketers = ['Mashrafe Bin Murtaza', 'Tamim Ikbal', 'Sakib Al Hasan', 'Mushfikur Rahim', 'Mahmudullah Riyad', 'Taskin', 'Towhid Hridoy'];
   return (
     <div className="App">
-      <Person name='Mashrafe Bin Murtaza' occupation='Former Captain of Bangladesh Cricket Team'></Person>
-      <Person name='Tamim Ikbal' occupation='ODI Captain of BD Cricket Team'></Person>
-      <Person name='Sakib Al Hasan' occupation='T20 Captain of BD Cricket Team'></Person>
-      <h5>New component</h5>
-      <Friend name='Shanto' studiesIn='Social Work'></Friend>
-      <Friend name='Alif' studiesIn='SWE'></Friend>
+      {
+        /* <Person name='Mashrafe Bin Murtaza' occupation='Former Captain of Bangladesh Cricket Team'></Person>
+        <Person name='Tamim Ikbal' occupation='ODI Captain of BD Cricket Team'></Person>
+        <Person name='Sakib Al Hasan' occupation='T20 Captain of BD Cricket Team'></Person>
+        <h5>New component</h5>
+        <Friend name='Shanto' studiesIn='Social Work'></Friend>
+        <Friend name='Alif' studiesIn='SWE'></Friend> */
+      }
+      {
+        // cricketers.map(cricketer => <ol><b>Name:</b> {cricketer} </ol>)
+      }
+      {
+        cricketers.map(cricketer => <Person name={cricketer}> </Person>)
+      }
+
+      {
+        // array of object theke data niye display
+        singers.map(singer => <Person name={singer.name}></Person>)
+      }
+
 
     </div>
   );
 }
 
 function Person(props) {
-  // console.log(props);
+  console.log(props);
   return (
     <div className="person ">
       <h1>{props.name}</h1>
-      <p>Profession: {props.occupation}</p>
+      {/* <p>{props.occupation}</p> */}
     </div>
   );
 }
