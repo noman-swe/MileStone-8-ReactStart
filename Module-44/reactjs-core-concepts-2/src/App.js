2c47e356ddd7e5d6dca5d20357502fe8a0610881
noman-swe <noman.swe130@gmail.com>
2023-03-19 01:14:18 +0600

    45-7 (advanced) State, component state hook and set state method

--- Module-44/reactjs-core-concepts-2/src/App.js
+++ Module-44/reactjs-core-concepts-2/src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    { name: 'laptop', price: 53000 },
-    { name: 'phone', price: 93000 },
-    { name: 'watch', price: 33000 },
-    { name: 'tablet', price: 5200 },
-  ];
+
   return (
     <div className="App">
-      {/* <Product title='Gun' price='5000'></Product>
-      <Product title='Glue' price='15154'></Product>
-      <Product title='Chair' price='9854'></Product> */}
-
-      {products.map(product => <Product name={product.name} price={product.price} > </Product>)}
-
+      <Counter></Counter>
     </div>
   );
 }
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  }
+
+  // Shorter the format
+  const decreaseCount = () => {
+    setCount(count - 1);
+  }
+  
+  return (
+    <div className="count">
+      <h1>Count:{count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  );
+}
+
+export default App;
+
+// 
+/* const products = [
+  { name: 'laptop', price: 53000 },
+  { name: 'phone', price: 93000 },
+  { name: 'watch', price: 33000 },
+  { name: 'tablet', price: 5200 },
+]; 
+
+{/* <Product title='Gun' price='5000'></Product>
+      <Product title='Glue' price='15154'></Product>
+      <Product title='Chair' price='9854'></Product> }
+
+      {products.map(product => <Product name={product.name} price={product.price} > </Product>)}
 
 // another component
 function Product(props) {
@@ -31,4 +60,4 @@ function Product(props) {
   );
 }
 
-export default App;
+*/
