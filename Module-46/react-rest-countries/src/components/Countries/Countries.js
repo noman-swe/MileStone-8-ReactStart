59018dd00d964a9ffde1b0787ef1f57b44d54e89
noman-swe <noman.swe130@gmail.com>
2023-03-22 00:31:53 +0600

    46-4 Components inside the folder structure and connect them

--- Module-46/react-rest-countries/src/components/Countries/Countries.js
+++ Module-46/react-rest-countries/src/components/Countries/Countries.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    }, [])
     return (
         <div>
-            <h2>Countries in other file.</h2>
+            <h1>Hello from Countries{countries.length}</h1>
+            {
+                countries.map(country => <Country countryName= {country.name.common}></Country>)
+            }
         </div>
     );
 };
---
+++ Module-46/react-rest-countries/src/components/Country/Country.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = (props) => {
+    return (
+        <div>
+            <h3>Country Name:{props.countryName}</h3>
+        </div>
+    );
+};
+
+export default Country;
