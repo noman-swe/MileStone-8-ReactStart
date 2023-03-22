8b3576a1ebd0cea8597077039ed9a37387dd233b
noman-swe <noman.swe130@gmail.com>
2023-03-22 20:59:30 +0600

    46-8 Build your react app and host your react app in Netlify

---
+++ Module-46/rest-countries-practice/src/components/Country/Country.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+    const { name, population, area, region, flags, startOfWeek } = props.country;
+    return (
+        <div className='country'>
+            <h2>Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p><small>Region: {region}</small></p>
+            <p><small>Start of week: {startOfWeek}</small></p>
+        </div>
+    );
+};
+
+export default Country;
