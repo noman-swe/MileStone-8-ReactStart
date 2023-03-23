4dc64843405645816973f34eb4f8f3e9121348d7
noman-swe <noman.swe130@gmail.com>
2023-03-23 13:44:30 +0600

    46_5-3 Load data using the useEffect and display data

--- Module-46/ExtraClass/reactjs-core-concepts-recap/src/App.js
+++ Module-46/ExtraClass/reactjs-core-concepts-recap/src/App.js
@@ -1,15 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <LoadPosts ></LoadPosts>
       <District name='Noakhali' special='Bivag'></District>
       <District name='Tangail' special='ChomChooom'></District>
       <District name='Sylhet' special='London'></District>
     </div>
   );
 }
+
+
+function LoadPosts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data))
+  }, [])
+
+  return (
+    <div className="load-posts">
+      <h3>Title:{posts.length}</h3>
+    {
+      posts.map(post=> <Post post={post}></Post>)
+    }
+    </div>
+  );
+}
+
+function Post (props) {
+  const {title, body} = props.post;
+  return(
+    <div className="posts" style={districtStyle}>
+      <h3>{title}</h3>
+      <p>{body}</p>
+    </div>
+    );
+}
+
 
 const districtStyle = {
   backgroundColor: 'gray',
@@ -20,7 +52,7 @@ const districtStyle = {
 
 function District(props) {
   const [power, setPower] = useState(1);
- 
+
   const boostPower = () => {
     const newPower = power * 2;
     setPower(newPower);
