3dc9eb48f5f3b987a17f0478dfc1d5bd6bdd7ffb
noman-swe <noman.swe130@gmail.com>
2023-03-25 18:12:13 +0600

    47_5-2 ES6 Modules, import default, relative path

--- Module-47/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
+++ Module-47/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { add } from '../../utilities/calculate';
+// import add from '../../utilities/calculate';
+
 
 const Cosmetics = () => {
+    const first = 55;
+    const second = 65;
+    const total = add(first, second)
     return (
         <div>
             <h3>This is cosmetics.</h3>
+            <p>{total}</p>
+
         </div>
     );
 };
---
+++ Module-47/react-storage-and-handler/src/components/Shoes/Shoes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { multiply } from '../../utilities/calculate';
+
+const Shoes = () => {
+    const jutaPrice = 20;
+    const muja = 5;
+    const totalJuta = multiply(jutaPrice, muja);
+
+    return (
+        <div>
+            <h3>This is shoes compo</h3>
+            <h5>Juta: {totalJuta}</h5>
+        </div>
+    );
+};
+
+export default Shoes;
