c8474b04178314812d2360dd2dd99d1e3513cc87
noman-swe <noman.swe130@gmail.com>
2023-03-25 17:55:21 +0600

    47_5-1 Module Introduction ES6 Modules simple import, export

---
+++ Module-47/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetics = () => {
+    return (
+        <div>
+            <h3>This is cosmetics.</h3>
+        </div>
+    );
+};
+
+export default Cosmetics;
