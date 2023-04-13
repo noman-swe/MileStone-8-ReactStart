7925edba09a2126bd43065d12d48f5e3099fad28
noman-swe <noman.swe130@gmail.com>
2023-04-14 00:28:20 +0600

    another practice of export default

--- Module-47/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
+++ Module-47/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import minus from '../../utilities/cal';
+
 import { add } from '../../utilities/calculate';
-// import add from '../../utilities/calculate';
+
 
 
 const Cosmetics = () => {
     const first = 55;
     const second = 65;
     const total = add(first, second)
+
+    const calMinus = minus(second, first)
     return (
         <div>
             <h3>This is cosmetics.</h3>
             <p>{total}</p>
+            <p>Minus = {calMinus}</p>
 
         </div>
     );
---
+++ Module-47/react-storage-and-handler/src/utilities/cal.js
@@ -0,0 +1,6 @@
+const minus = (num1, num2) => { 
+    const minus = num1 - num2;
+    return minus;
+}
+
+export default minus;
