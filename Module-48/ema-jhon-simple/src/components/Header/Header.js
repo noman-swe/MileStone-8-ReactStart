07ccc5e76fa10a02fa1f024521f9a927a1c8046f
noman-swe <noman.swe130@gmail.com>
2023-05-03 13:55:15 +0600

    48-2 Header Component, logo position, set nav menut -m

--- Module-48/ema-jhon-simple/src/components/Header/Header.js
+++ Module-48/ema-jhon-simple/src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt=''/>
+            <div className="links">
+                <a href="/shop">Shop</a>
+                <a href="orders">Orders</a>
+                <a href="inventory">Inventory</a>
+                <a href="About">About</a>
+            </div>
+        </nav>
     );
 };
 
