1425f49cea76f7550da4ab0b1fb99c350d86a16c
noman-swe <noman.swe130@gmail.com>
2023-08-29 01:21:45 +0600

    Edit of comments for 48-7

--- Module-48-new/ema-jhon-simple/src/components/Shop/Shop.js
+++ Module-48-new/ema-jhon-simple/src/components/Shop/Shop.js
@@ -4,7 +4,8 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
-    // 
+
+    // state declaration coz of adding new things 
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
@@ -16,7 +17,7 @@ const Shop = () => {
     // handler ta ekhan e add korte hoisy coz amra data upor theke nichy pathate pari but nichy theke upore pathaite pari na. and chaile event handler taw pathate pari.
     const handleAddToCart = (product) => {
         console.log(product);
-        // 
+        // ei handleAddToCart e press korar por new state update hocchy tai ei-khane update korte hbe tarpor seta product.js er cart section e show korte hobe. 
         const newCart = [...cart, product];
         setCart(newCart);
     }
